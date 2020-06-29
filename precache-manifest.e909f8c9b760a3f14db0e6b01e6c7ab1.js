20a431c5a2682dded8919ac61aafec043b33d30b
Jason Warta <jasonwarta@gmail.com>
2020-06-29 15:40:32 -0800

    Updates

---
+++ precache-manifest.e909f8c9b760a3f14db0e6b01e6c7ab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84800fb8e1e3ca68d2db2a4f28a11c9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d87a6d7d063ac60d1f3d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a56fcbe0b11ce4886e1a",
+    "url": "/static/js/2.1d2e595e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d2e595e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d87a6d7d063ac60d1f3d",
+    "url": "/static/js/main.f2c392a1.chunk.js"
+  },
+  {
+    "revision": "267c76d368cd3741278b",
+    "url": "/static/js/runtime-main.1a0e39b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
